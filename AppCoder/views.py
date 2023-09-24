1488f5311af89f90df66882c8b59bc867bd20e89
Gabriel Galiano <gabrieljjgaliano@gmail.com>
2023-09-24 11:14:29 -0300

    new def add (profe_nuevo) in views

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Profesor
 
 # Create your views here.
+
+def profe_nuevo(request):
+    profeN = Profesor(nombre="Juan", apellido="Perez", email="q2Z0u@example.com", profesion="Ingeniero")
+    profeN.save()
+
+    return HttpResponse('Hemos guardado al profesor {}')
+
+

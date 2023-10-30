f522c6e7504b45949476b3795b4935eaced105f0
Gabriel Galiano <gabrieljjgaliano@gmail.com>
2023-10-30 00:08:53 -0300

    .

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView
+from django.contrib.auth.forms import AuthenticationForm
 from AppCoder.models import *
 from .forms import *
 
@@ -257,3 +258,18 @@ def resultado_entregable(request):
 
 class EstudianteLista(ListView):
     model = Estudiante
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('AppCoder:inicio')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'AppCoder/login.html', {'form': form})
